4fe28a536b3e0c4793ef194bca208e639aa09620
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-12-07 21:13:37 +0100

    Preserve state

--- gruppeweb.galleri.js
+++ gruppeweb.galleri.js
@@ -1,9 +1,13 @@
 (function($) {
+	//Set host, get base URLs for resources
 	var host = "http://itu.dk/people/nroe/etc/";
 	var baseUrl = location.href.split("/")[2];
 	var requestUrl = host+"gallerifetch.php?base="+baseUrl;
 
-	//By CMS http://stackoverflow.com/questions/647259/javascript-query-string
+	/**
+	 * By CMS http://stackoverflow.com/questions/647259/javascript-query-string
+	 * Returns the "search" (query) part of URL as a map from attribute to value.
+	 */
 	function getQueryString() {
 	  var result = {}, queryString = location.search.substring(1),
 		  re = /([^&=]+)=([^&]*)/g, m;
@@ -16,13 +20,17 @@
 	}
 	
 	var urlQuery = getQueryString();
+	if(!isNaN(urlQuery['id'])) {
+		getContent(parseInt(urlQuery['id']));
+		setUpClickListeners();
+	}
 	
-	function getContent(id) {
+	function getContent(id, isPop) {
+		$("#loading-box").fadeIn('slow');
 		$.getJSON(requestUrl+"&id="+id+"&callback=?", function(data2) {
 			$("#content").slideUp('slow', function() {
-				$("#loading-box").fadeIn('slow');
 				$(this).html(data2.content).slideDown('slow');
-				history.pushState({ thisId: id }, $('title').text(), "?id="+id);
+				if(!isPop) history.pushState({ thisId: id }, $('title').text(), "?id="+id);
 				urlQuery = getQueryString();
 				$("#views-slideshow-galleria-images-1").each(function() {
 					$(this).css('width','100%').css('height','400px').
@@ -38,13 +46,27 @@
 		});
 	}
 	
+	function setUpClickListeners() {
+		$("#content a").live('click', function() {
+			var href = $(this).attr('href');
+			if(href.indexOf("image/tid") > -1) {
+				var id = href.split("/")[3];
+				getContent(id);
+				return false;
+			}
+		});
+		$('.galleria-images .galleria-image img').live('click', function() {
+			window.open($(this).attr('src'), '_blank');
+		});
+	}
+	
 	window.onpopstate = function() {
 		urlQuery = getQueryString();
-		if(isNaN(urlQuery['id'])) getContent(0);
+		if(isNaN(urlQuery['id'])) getContent(0, true);
 		else {
 			var id = parseInt(urlQuery['id']);
 			if(id > -1) {
-				getContent(id);
+				getContent(id, true);
 			}
 		}
 	}
@@ -68,17 +90,7 @@
 				$("#content").slideUp('slow', function() {
 					$(this).html(data).slideDown('slow');
 					$("#activate-gallery").hide();
-					$("#content a").live('click', function() {
-						var href = $(this).attr('href');
-						if(href.indexOf("image/tid") > -1) {
-							var id = href.split("/")[3];
-							getContent(id);
-							return false;
-						}
-					});
-					$('.galleria-images .galleria-image img').live('click', function() {
-						window.open($(this).attr('src'), '_blank');
-					});
+					setUpClickListeners();
 				});
 			});
 		});
